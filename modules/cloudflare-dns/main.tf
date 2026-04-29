5eca4728739562041039f2fde8d240e090cac1ed
unknown <jamesadewara1@gmail.com>
2026-04-29 13:34:12 +0100

    added bucket arn to output and fixed cloudflare 4 syntax and a placeholder.zip for lambda

--- modules/cloudflare-dns/main.tf
+++ modules/cloudflare-dns/main.tf
@@ -16,14 +16,11 @@ resource "cloudflare_record" "site" {
   proxied = true
 }
 
-resource "cloudflare_zone_setting" "ssl" {
-  zone_id    = var.zone_id
-  setting_id = "ssl"
-  value      = "flexible"
-}
-
-resource "cloudflare_zone_setting" "always_use_https" {
-  zone_id    = var.zone_id
-  setting_id = "always_use_https"
-  value      = "on"
+resource "cloudflare_zone_settings_override" "settings" {
+  zone_id = var.zone_id
+  settings {
+    ssl              = "flexible"
+    always_use_https = "on"
+    min_tls_version  = "1.2"
+  }
 }
